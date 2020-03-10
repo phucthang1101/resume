5707aba637c5d8fd3dbf948139e0499471a2106b
phucthang1101 <phucthangvt1101@gmail.com>
2020-03-11 00:34:39 +0700

    Updates

---
+++ precache-manifest.36f955f91e6a6041fd6be73caf0cb578.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa12b49bac5c4f8903106556501e2c4",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "11320dd8f2a239b7d58e",
+    "url": "/resume/static/css/2.7190192a.chunk.css"
+  },
+  {
+    "revision": "4cbf58d269e31e347f47",
+    "url": "/resume/static/css/main.531e1e0a.chunk.css"
+  },
+  {
+    "revision": "11320dd8f2a239b7d58e",
+    "url": "/resume/static/js/2.18a030e1.chunk.js"
+  },
+  {
+    "revision": "18e4d2fb036def2d15872b9827719f91",
+    "url": "/resume/static/js/2.18a030e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cbf58d269e31e347f47",
+    "url": "/resume/static/js/main.e260052a.chunk.js"
+  },
+  {
+    "revision": "e84638710184c42eba35",
+    "url": "/resume/static/js/runtime-main.114f2df9.js"
+  },
+  {
+    "revision": "465244bc2d207c2cd8b120bbb737bcaf",
+    "url": "/resume/static/media/BannerImage.465244bc.png"
+  },
+  {
+    "revision": "331a53808bcbd0348239e053a7d1eaff",
+    "url": "/resume/static/media/ExpenseApp.331a5380.png"
+  },
+  {
+    "revision": "c022c4a31b300d9c8245fbb3f55c94b0",
+    "url": "/resume/static/media/NodeJS1.c022c4a3.png"
+  },
+  {
+    "revision": "96ecab62fbd44005af6e685b141931b5",
+    "url": "/resume/static/media/avatar.96ecab62.jpg"
+  },
+  {
+    "revision": "e704f8bf6c85ee70e36b8cdf824a6cf0",
+    "url": "/resume/static/media/personalWebsite.e704f8bf.png"
+  },
+  {
+    "revision": "ddac9bec350cbef8deb5716239a4244b",
+    "url": "/resume/static/media/programmers.ddac9bec.jpg"
+  },
+  {
+    "revision": "044d1a04a2d316f296a2a5ec9ae4682a",
+    "url": "/resume/static/media/project1.044d1a04.png"
+  },
+  {
+    "revision": "34be6f6b2091fcd7689dbdd38e4a824b",
+    "url": "/resume/static/media/project2.34be6f6b.png"
+  },
+  {
+    "revision": "13dfc97a53aeaa3204372d5d0a31813c",
+    "url": "/resume/static/media/project3.13dfc97a.png"
+  }
+]);
